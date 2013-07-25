86f44b3aeb6432a1e7370d572aeb0f3bb2b47fb6
Peter Lind <peter.e.lind@gmail.com>
2013-07-25 21:32:22 +0200

    Adding an A/B test for operator precedence

--- js.js
+++ js.js
@@ -1,78 +1,200 @@
 var precedence = {
-    running: false,
-    times_run: 0,
-    times_correct: 0,
-    button: null,
-    list: null,
-    scoreboard: null,
+        running: false,
+        times_run: 0,
+        times_correct: 0,
+        button: null,
+        list: null,
+        scoreboard: null,
 
-    interaction: function(e) {
-        if (this.running) {
-            this.stopTest();
-        } else {
-            this.startTest();
-        }
-    },
-
-    stopTest: function() {
-        var failed = false;
-
-        this.running = false;
-        this.button.text('Start');
-
-        this.list.children().each(function(idx, item) {
-            var self = $(this);
-            if (idx == parseInt(self.data('index'), 10) - 1) {
-                self.css('background-color', '#afa');
+        interaction: function(e) {
+            if (this.running) {
+                this.stopTest();
             } else {
-                self.css('background-color', '#faa');
-                failed = true;
+                this.startTest();
             }
-        });
+        },
 
-        if (!failed) {
-            this.times_correct++;
+        stopTest: function() {
+            var failed = false;
+
+            this.running = false;
+            this.button.text('Start');
+
+            this.list.children().each(function(idx, item) {
+                var self = $(this);
+                if (idx == parseInt(self.data('index'), 10) - 1) {
+                    self.css('background-color', '#afa');
+                } else {
+                    self.css('background-color', '#faa');
+                    failed = true;
+                }
+            });
+
+            if (!failed) {
+                this.times_correct++;
+            }
+
+            this.updateScoreboard();
+        },
+
+        startTest: function() {
+            var items;
+
+            this.running = true;
+
+            this.times_run++;
+            this.button.text('Check');
+
+            items = this.list.children().css('background-color', 'transparent').detach();
+            items.sort(function(a, b) {
+                return Math.random() > 0.5 ? 1 : 0;
+            });
+
+            this.list.append(items);
+        },
+
+        updateScoreboard: function() {
+            this.scoreboard.text("Correct / tests: " + this.times_correct + ' / ' + this.times_run);
+        },
+
+        init: function(list_id, button_id, scoreboard_id) {
+            var that = this;
+
+            this.button     = $('#' + button_id);
+            this.list       = $('#' + list_id);
+            this.scoreboard = $('#' + scoreboard_id);
+
+            this.list.sortable();
+            this.button.click(function(e) {
+                that.interaction(e);
+            });
+
+            this.updateScoreboard();
         }
-
-        this.updateScoreboard();
     },
+    precedence_ab = {
+        test_a: null,
+        test_b: null,
+        scoreboard: null,
+        showing_answer: false,
+        base_height: 0,
+        current_winner: 0,
+        times_run: 0,
+        times_correct: 0,
+        operators: {
+            0: "clone, new",
+            1: "[",
+            2: "++, --, ~, (bool), (int), (string), (float), (object), (array), (unset), @",
+            3: "instanceof",
+            4: "!",
+            5: "*, /, %",
+            6: "+, -, .",
+            7: "&lt;&lt;, &gt;&gt;",
+            8: "&lt;, &lt;=, &gt;=, &gt;",
+            9: "==, ===, !=, !==, &lt;&gt;",
+            10: "&amp;",
+            11: "^",
+            12: "|",
+            13: "&amp;&amp;",
+            14: "||",
+            15: "? :",
+            16: "=, +=, -=, *=, /=, %= &lt;&lt;=, &gt;&gt;=, .=, &amp;=, |=, ^=, =&gt;",
+            17: "and",
+            18: "xor",
+            19: "or",
+            20: ","
+        },
 
-    startTest: function() {
-        var items;
+        setupTest: function() {
+            var a = Math.floor(Math.random() * 20),
+                b;
 
-        this.running = true;
+            do {
+                b = Math.floor(Math.random() * 20);
+            } while (a == b);
 
-        this.times_run++;
-        this.button.text('Check');
+            this.test_a.html(this.operators[a])
+                .attr('data-index', a);
 
-        items = this.list.children().css('background-color', 'transparent').detach();
-        items.sort(function(a, b) {
-            return Math.random() > 0.5 ? 1 : 0;
-        });
+            this.test_b.html(this.operators[b])
+                .attr('data-index', b);
 
-        this.list.append(items);
-    },
+            if (a == 2 || a == 16) {
+                this.test_a.css('line-height', '5em');
+            } else {
+                this.test_a.css('line-height', '10em');
+            }
 
-    updateScoreboard: function() {
-        this.scoreboard.text("Correct / tests: " + this.times_correct + ' / ' + this.times_run);
-    },
+            if (b == 2 || b == 16) {
+                this.test_b.css('line-height', '5em');
+            } else {
+                this.test_b.css('line-height', '10em');
+            }
 
-    init: function(list_id, button_id, scoreboard_id) {
-        var that = this;
+            this.current_winner = a < b ? a : b;
+        },
 
-        this.button     = $('#' + button_id);
-        this.list       = $('#' + list_id);
-        this.scoreboard = $('#' + scoreboard_id);
+        reset: function() {
+            this.showing_answer = false;
+            this.test_a.css('background-color', '#fff');
+            this.test_b.css('background-color', '#fff');
 
-        this.list.sortable();
-        this.button.click(function(e) {
-            that.interaction(e);
-        });
+            this.setupTest();
+        },
 
-        this.updateScoreboard();
-    }
-};
+        checkChoice: function(e) {
+            var that    = this,
+                element = $(e.target);
+
+            if (this.showing_answer) {
+                return;
+            }
+
+            this.showing_answer = true;
+
+            window.setTimeout(function() {
+                that.reset();
+            }, 3000);
+
+            if (parseInt(element.attr('data-index'), 10) == this.current_winner) {
+                element.css('background-color', '#afa');
+                this.times_correct++;
+            } else {
+                element.css('background-color', '#faa');
+
+                if (element.prop('id') == 'test_a') {
+                    this.test_b.css('background-color', '#afa');
+                } else {
+                    this.test_a.css('background-color', '#afa');
+                }
+            }
+
+            this.times_run++;
+            this.updateScoreboard();
+        },
+
+        updateScoreboard: function() {
+            this.scoreboard.text("Correct / tests: " + this.times_correct + ' / ' + this.times_run);
+        },
+
+        init: function(test_a_id, test_b_id, scoreboard_id) {
+            var that = this;
+
+            this.test_a     = $('#' + test_a_id);
+            this.test_b     = $('#' + test_b_id);
+            this.scoreboard = $('#' + scoreboard_id);
+
+            this.test_a.parent().on('click', '#' + test_a_id + ', #' + test_b_id, function(e) {
+                that.checkChoice(e);
+            });
+
+            this.base_height = this.test_a.height();
+            this.setupTest();
+            this.updateScoreboard();
+        }
+    };
 
 $(function() {
     precedence.init('precedence-list', 'precedence', 'precedence-scores');
+    precedence_ab.init('test_a', 'test_b', 'ab-scores');
 });
